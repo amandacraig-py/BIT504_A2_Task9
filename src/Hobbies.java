aed8bb784086a0aedf9b76b60512d93346621c32
Amanda Craig <school4craigs@gmail.co.nz>
2026-01-02 22:50:03 +1300

    Initial commit - Added MyHobbies class

---
+++ src/Hobbies.java
@@ -0,0 +1,10 @@
+
+public class Hobbies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("BIT504 Assessment 2 - Task 9");
+        System.out.println("I like reading. This is my first commit!");
+	}
+
+}
---
+++ src/MyHobbies.java
@@ -0,0 +1,10 @@
+
+public class MyHobbies {
+
+	public static void main(String[] args) {
+		System.out.println("BIT504 Assessment 2 - Task 9");
+		System.out.println("I like reading. This is my first commit");
+
+	}
+
+}
