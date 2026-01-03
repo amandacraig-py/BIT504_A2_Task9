67e4a3b276e79b827e24035647e3c5b911454809
Amanda Craig <school4craigs@gmail.co.nz>
2026-01-03 14:57:31 +1300

    Second commit - Added seventh println statement

--- src/MyHobbies.java
+++ src/MyHobbies.java
@@ -4,7 +4,7 @@ public class MyHobbies {
 	public static void main(String[] args) {
 		System.out.println("BIT504 Assessment 2 - Task 9");
 		System.out.println("I like reading. This is my first commit");
-
+		System.out.println("I like listening to music. This is my second commit");
 	}
 
 }
