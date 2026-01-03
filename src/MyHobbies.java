10477955ce1278ee92f6a55b5321415dd94c86b0
Amanda Craig <school4craigs@gmail.co.nz>
2026-01-03 15:08:31 +1300

    Third & final commit - Added 8th and 9th println statements

--- src/MyHobbies.java
+++ src/MyHobbies.java
@@ -5,6 +5,8 @@ public class MyHobbies {
 		System.out.println("BIT504 Assessment 2 - Task 9");
 		System.out.println("I like reading. This is my first commit");
 		System.out.println("I like listening to music. This is my second commit");
+		System.out.println("\n");
+		System.out.println("I like creating art. This is my third and final commit");
 	}
 
 }
